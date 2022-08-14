352c41d87fff3c932f4fbd40b6df334bf6db91e1
raselASrock <mdraselm413@gmail.com>
2022-08-14 20:15:50 +0600

    second-commit

--- js/event.js
+++ js/event.js
@@ -1,7 +1,7 @@
 console.log("this is separate JS file");
 
 // Option: 1 directly set on the HTML element
-
+{/* <button onclick="console.log(5)">Another Button </button> */ }
 
 // Option: 2 add click function
 function makeRed() {
@@ -18,3 +18,12 @@ const purpleButton = document.getElementById('make-purple');
 purpleButton.onclick = function makePurple() {
     document.body.style.backgroundColor = 'purple'
 }
+
+// Option: 4
+const pinkButton = document.getElementById('make-pink')
+pinkButton.addEventListener('click', makePink)
+function makePink() {
+    document.body.style.backgroundColor = 'pink';
+}
+
+// Option: 4 Another
